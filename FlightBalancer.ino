33e3bcc5f859c27409f5676a44c76b35c94324f9
Oren Online <zaki.bm@gmail.com>
2019-09-21 11:03:22 +0800

    Update FlightBalancer.ino

--- FlightBalancer.ino
+++ FlightBalancer.ino
@@ -15,7 +15,7 @@ LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 // Define pins and max distance:
 #define trigPin  8
 #define echoPin  9
-#define MAX_DISTANCE 350 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+#define MAX_DISTANCE 100 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 
 NewPing sonar(trigPin, echoPin, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 float duration, distance;
@@ -26,7 +26,6 @@ void setup()
     
     lcd.begin(16,2);//Defining 16 columns and 2 rows of lcd display
     lcd.backlight();//To Power ON the back light
-    //lcd.backlight();// To Power OFF the back light
 
     pinMode(BUZZER, OUTPUT);
 
@@ -43,17 +42,16 @@ void loop()
 {
     checkDistance();  //check distance 
         
-    while(distance >= 50)
+    while(distance >= 8)
     {
-        //lcd.clear();
         lcd.setCursor(0,0); //Defining positon to write from first row,first column .
         lcd.print("Still Far...    "); 
         delay(1000); 
 
         lcd.setCursor(0,1);  
-        lcd.print("Distance = ");
+        lcd.print("Dist = ");
         lcd.print(distance); // Prints the measured distance
-        lcd.print(" cm");
+        lcd.print(" cm ");
 
         //LED Green Light Up
         digitalWrite(LIGHT_GREEN,HIGH);
@@ -67,16 +65,16 @@ void loop()
         checkDistance();       
     }
 
-    while(distance >= 40 && distance < 50)
+    while(distance >= 6 && distance < 8)
     {
         lcd.setCursor(0,0); //Defining positon to write from first row,first column .
         lcd.print("Getting Near... "); 
         delay(1000); 
 
         lcd.setCursor(0,1);  
-        lcd.print("Distance = ");
+        lcd.print("Dist = ");
         lcd.print(distance); // Prints the measured distance
-        lcd.print(" cm");
+        lcd.print(" cm ");
 
         //LED Green Light Up
         digitalWrite(LIGHT_GREEN,HIGH);
@@ -90,16 +88,16 @@ void loop()
         checkDistance();       
     }
 
-    while(distance >= 30 && distance < 40)
+    while(distance >= 4 && distance < 6)
     {
         lcd.setCursor(0,0); //Defining positon to write from first row,first column .
         lcd.print("Nearer...       "); 
         delay(1000); 
 
         lcd.setCursor(0,1);  
-        lcd.print("Distance = ");
+        lcd.print("Dist = ");
         lcd.print(distance); // Prints the measured distance
-        lcd.print(" cm");
+        lcd.print(" cm ");
 
         //LED Yellow Light Up
         digitalWrite(LIGHT_GREEN,LOW);
@@ -107,16 +105,16 @@ void loop()
         digitalWrite(LIGHT_RED,LOW);
 
         //Sound buzzer?
-        //playBuzzerOnce();
+        playBuzzerOneSecond();
 
         //check distance again
         checkDistance();       
     }
 
-    while(distance >= 20 && distance < 30)
+    /*while(distance >= 20 && distance < 30)
     {
         lcd.setCursor(0,0); //Defining positon to write from first row,first column .
-        lcd.print("Almost...       "); 
+        lcd.print("Almost..."); 
         delay(1000); 
 
         lcd.setCursor(0,1);  
@@ -134,18 +132,18 @@ void loop()
 
         //check distance again
         checkDistance();       
-    }
+    }*/
 
-    while(distance >= 10 && distance < 20)
+    while(distance > 0 && distance < 4)
     {
         lcd.setCursor(0,0); //Defining positon to write from first row,first column .
         lcd.print("Spot On...      "); 
         delay(1000); 
 
         lcd.setCursor(0,1);  
-        lcd.print("Distance = ");
+        lcd.print("Dist = ");
         lcd.print(distance); // Prints the measured distance
-        lcd.print(" cm");
+        lcd.print(" cm ");
 
         //LED Yellow Light Up
         digitalWrite(LIGHT_GREEN,LOW);
@@ -153,7 +151,7 @@ void loop()
         digitalWrite(LIGHT_RED,HIGH);
 
         //Sound buzzer?
-        playBuzzerOnce();
+        playBuzzerCont();
 
         //check distance again
         checkDistance();       
@@ -171,10 +169,16 @@ void checkDistance(void){
     delay(500); // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.  
 }
 
-void playBuzzerOnce(void){
+void playBuzzerOneSecond(void){
     NewTone(BUZZER, 1000); // Send 1KHz sound signal...
     delay(1000);        // ...for 1 sec
     noNewTone(BUZZER);     // Stop sound...
     delay(1000);        // ...for 1sec
 }
 
+void playBuzzerCont(void){
+    NewTone(BUZZER, 1000); // Send 1KHz sound signal...
+    delay(5000);        // ...for 1 sec
+    noNewTone(BUZZER);     // Stop sound...
+    delay(1000);        // ...for 1sec
+}
